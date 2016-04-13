b4119938cdc41c111c3531b156903aedf21f1c99
Hackbright Student <info@hackbrightacademy.com>
2016-04-13 16:07:16 -0700

    Complete post viewing solution

--- markov.py
+++ markov.py
@@ -54,47 +54,23 @@ def make_chains(text_string):
 def make_text(chains):
     """Takes dictionary of markov chains; returns random text."""
 
-    #text = empty string to put all our text in eventually
-    text = ""
-    #the initial key is a random choice (see import random choice at top of program)
-    #the only random key we will have
-    initial_key = choice(chains.keys())
-    #grabs an random value from the list of values for the initial key
-    initial_value = choice(chains[initial_key])
-    # print initial_key
-    # print initial_value
-    #adds the initial_key and initial_value to the text string with spaces
-    #use join because initial_key is a tuple
-    text = " ".join(initial_key) + " " + initial_value
-    #text = text + " " + initial_value
-    print text
-    
-    #sets up next key as a tuple made from inital_key index 1 and initial value
-    next_key = (initial_key[1], initial_value)
-    print next_key
+    key = choice(chains.keys())
+    words = [key[0], key[1]]
+    while key in chains:
+        # Keep looping until we have a key that isn't in the chains
+        # (which would mean it was the end of our original text)
+        #
+        # Note that for long texts (like a full book), this might mean
+        # it would run for a very long time.
+    #choosing random value and binding it to word
+        word = choice(chains[key])
+    #appending word(random value) to words (our initial key tuple)
+        words.append(word)
+    #binding key to the new tuple created from key index 1 and random value
+        key = (key[1], word)
 
-    
-    while True:
-        current_key = next_key
+    return " ".join(words)
 
-        if current_key in chains:
-
-
-        else:
-            False
-    # and then look for them as a key in the dictionary
-    # then check for that new key in the chains dictionary
-    # and if its there chose a random value from that key
-    # and got through the above again and again....
-    # code idea below
-    # while initial_key in chains:  
-    for new_key in chains:
-            chains[new_key] = chains.get(choice([initial_key])
-            (new_key) + initial_value
-
-    each time we go throuhg the loop, if the new key is in th dictionary
-    we want to choose a random value from that key and add the items to the text 
-    return text
 
 
 input_path = "green-eggs.txt"
@@ -108,4 +84,48 @@ chains = make_chains(input_text)
 # Produce random text
 py = make_text(chains)
 
-#print random_text
+print py
+
+
+
+ # #text = empty string to put all our text in eventually
+ #    text = ""
+ #    #the initial key is a random choice (see import random choice at top of program)
+ #    #the only random key we will have
+ #    initial_key = choice(chains.keys())
+ #    #grabs an random value from the list of values for the initial key
+ #    initial_value = choice(chains[initial_key])
+ #    # print initial_key
+ #    # print initial_value
+ #    #adds the initial_key and initial_value to the text string with spaces
+ #    #use join because initial_key is a tuple
+ #    text = " ".join(initial_key) + " " + initial_value
+ #    #text = text + " " + initial_value
+ #    print text
+    
+ #    #sets up next key as a tuple made from inital_key index 1 and initial value
+ #    next_key = (initial_key[1], initial_value)
+ #    print next_key
+
+   
+ #    while True:
+ #        current_key = next_key
+
+ #        if current_key in chains:
+
+
+ #        else:
+ #            False
+ #    # and then look for them as a key in the dictionary
+ #    # then check for that new key in the chains dictionary
+ #    # and if its there chose a random value from that key
+ #    # and got through the above again and again....
+ #    # code idea below
+ #    # while initial_key in chains:  
+ #    for new_key in chains:
+ #            chains[new_key] = chains.get(choice([initial_key])
+ #            (new_key) + initial_value
+
+ #    each time we go throuhg the loop, if the new key is in th dictionary
+ #    we want to choose a random value from that key and add the items to the text 
+ #    return text
